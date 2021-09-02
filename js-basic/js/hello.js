13bf5e24142b8964e9fbae61332fa7cd6c717713
RFG\r23600252 <Junnosuke.Yonezawa@jp.ricoh.com>
2021-09-02 14:12:34 +0900

    no message

--- js-basic/js/hello.js
+++ js-basic/js/hello.js
@@ -3,10 +3,12 @@ const func = () => {
     console.log(dt.getMonth()+1);
     console.log(dt.getDate());
     dt.setDate(dt.getDate() - 1);
+    today = dt.getFullYear()+"/"+(dt.getMonth()+1)+"/"+dt.getDate();
 }
 
 var dt = new Date();
 dt.setFullYear(2021);
+var today = dt.getFullYear()+"/"+(dt.getMonth()+1)+"/"+dt.getDate();
 
 //月
 //1月が0、12月が11。
@@ -16,6 +18,7 @@ dt.setMonth(7);
 dt.setDate(27);
 
 document.body.style.backgroundColor = "#ccffcc";
+document.write(today);
 //曜日
 //日曜が0、土曜日が6。
 //dt.getDay(1)
